96cb8348e954164fb310fc1b3c95219b9d02ded2
Stone Comstock <comstockstone@yahoo.com>
2022-06-19 16:25:32 -0700

    added axios api request to trending page

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';;
 
 // component for trending page
+// will fetch all trending titles and display cards for each one
 const Trending = () => {
+  const [content, setContent] = useState([]);
+
+  // fetch trending movies from api
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_APIKEY}`
+    );
+    console.log(data.results);
+    setContent(data.results);
+  };
+
+  useEffect(() => {
+    fetchTrending();
+  }, []);
+
   return (
     <div>
       <span className='pageTitle'>Trending</span>
