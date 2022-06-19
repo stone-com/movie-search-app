92c8902984572e81c4e1882ad88dbe902e53f440
Stone Comstock <comstockstone@yahoo.com>
2022-06-19 13:57:38 -0700

    Started routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
 import React from 'react';
 import Header from './components/Header/Header';
+import Trending from './Pages/Trending/Trending';
+import Movies from './Pages/Movies/Movies';
+import Series from './Pages/Series/Series';
+import Search from './Pages/Search/Search';
 import SimpleBottomNavigation from './components/MainNav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Container } from '@mui/system';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <div className='app'></div>
+      <div className='app'>
+        <Container>
+          {/* routes for the 4 main pages/components */}
+          <Routes>
+            <Route path='/' element={<Trending />} exact />
+            <Route path='/movies' element={<Movies />} />
+            <Route path='/series' element={<Series />} />
+            <Route path='/search' element={<Search />} />
+          </Routes>
+        </Container>
+      </div>
+
       <SimpleBottomNavigation />
-    </>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Trending/Trending.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// component for trending page
+const Trending = () => {
+  return (
+    <div>
+      <span className='pageTitle'>Trending</span>
+    </div>
+  );
+};
+
+export default Trending;
