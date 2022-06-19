e2e63feebce5bd7c8b1515b332d5a2d5f0ed2ae7
Stone Comstock <comstockstone@yahoo.com>
2022-06-19 12:45:32 -0700

    deleted some starter files and added header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Header.css';
+
+// component for header bar at top of page
+const Header = () => {
+  return <span className='header'>Find a Show or Movie</span>;
+};
+
+export default Header;
