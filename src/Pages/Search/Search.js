f99aa1bb3f20afefaf226107ac7aaf735bba56a7
Stone Comstock <comstockstone@yahoo.com>
2022-06-19 21:57:01 -0700

    search feature working

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -4,6 +4,9 @@ import TextField from '@mui/material/TextField';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Button, Tab, Tabs } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import SingleContentCard from '../../components/SingleContent/SingleContentCard';
+import CustomPagination from '../../components/Pagination/CustomPagination';
+import './Search.css';
 
 // dark theme for search bar for because it looks better/easier to see
 const darkTheme = createTheme({
@@ -13,60 +16,87 @@ const darkTheme = createTheme({
 });
 
 const Search = () => {
-  const [type, setType] = useState();
-  const [searchText, setSearchText] = useState();
+  const [type, setType] = useState(0);
+  const [searchText, setSearchText] = useState("");
   const [page, setPage] = useState(1);
-  const [content, setContent] = useState();
+  const [content, setContent] = useState([]);
   const [numberOfPages, setNumberOfPages] = useState();
 
   const fetchSearch = async () => {
-    const { data } = await axios.get(
-      // if type is 0 (falsy) use tv, but if it is 1(truthy) use movie
-      // type value is changed when active tab is changed.
-      `https://api.themoviedb.org/3/search/${type ? 'tv' : 'movie'}?api_key=${
-        process.env.REACT_APP_APIKEY
-      }&language=en-US&query=${searchText}&page=${page}&include_adult=false`
-    );
-
-    setContent(data.results);
-    setNumberOfPages(data.total_pages);
-
-    useEffect(() => {
-      window.scroll(0, 0);
-      fetchSearch();
-    }, [type, page]);
+    try {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${
+          process.env.REACT_APP_APIKEY
+        }&language=en-US&query=${searchText}&page=${page}&include_adult=false`
+      );
+      setContent(data.results);
+      setNumberOfPages(data.total_pages);
+      // console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchSearch();
+    // eslint-disable-next-line
+  }, [type, page]);
 
   return (
     <div>
       <ThemeProvider theme={darkTheme}>
-        <div style={{ display: 'flex', margin: '15px 0' }}>
+        <div className="search">
           <TextField
             style={{ flex: 1 }}
-            className='searchBox'
-            label='Search'
-            variant='outlined'
+            className="searchBox"
+            label="Search"
+            variant="filled"
             onChange={(e) => setSearchText(e.target.value)}
           />
-          <Button variant='contained' style={{ marginLeft: 10 }}>
-            <SearchIcon />
+          <Button
+            onClick={fetchSearch}
+            variant="contained"
+            style={{ marginLeft: 10 }}
+          >
+            <SearchIcon fontSize="large" />
           </Button>
         </div>
         <Tabs
           value={type}
-          indicatorColor='primary'
-          textColor='primary'
-          // the value when a tab is clicked is a number, starting at 0
-          onChange={(e, newValue) => {
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={(event, newValue) => {
             setType(newValue);
             setPage(1);
           }}
           style={{ paddingBottom: 5 }}
+          aria-label="disabled tabs example"
         >
-          <Tab style={{ width: '50%' }} label='Search for Movies' />
-          <Tab style={{ width: '50%' }} label='Search for Shows' />
+          <Tab style={{ width: "50%" }} label="Search Movies" />
+          <Tab style={{ width: "50%" }} label="Search TV Series" />
         </Tabs>
       </ThemeProvider>
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContentCard
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type={type ? "tv" : "movie"}
+              vote_average={c.vote_average}
+            />
+          ))}
+        {searchText &&
+          !content &&
+          (type ? <h2>No Series Found</h2> : <h2>No Movies Found</h2>)}
+      </div>
+      {numberOfPages > 1 && (
+        <CustomPagination setPage={setPage} numberOfPages={numberOfPages} />
+      )}
     </div>
   );
 };
