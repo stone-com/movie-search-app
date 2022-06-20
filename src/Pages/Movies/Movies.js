2f41de1e7c985d39684b4b3d9da2fea3d5399dce
Stone Comstock <comstockstone@yahoo.com>
2022-06-19 19:19:00 -0700

    starting movie discover page

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 const Movies = () => {
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [numberOfPages, setNumberOfPages] = useState();
+
+  const fetchMovies = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+    );
+    // set content state to results array from data
+    setContent(data.results);
+    setNumberOfPages(data.total_pages);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
   return (
     <div>
-      <span className="pageTitle">Movies</span>
+      <span className='pageTitle'>Movies</span>
     </div>
-  )
-}
+  );
+};
 
-export default Movies
+export default Movies;
